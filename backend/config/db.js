0044835e8a420dac6ad17d10ba660dafad7b82f7
MissionPossible2025 <n16945683@gmail.com>
2026-04-06 14:58:59 +0530

    from kolaru

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     console.log("Trying to connect to MongoDB...");
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log("DB:", conn.connection.name);
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1); // Stop the server if connection fails
